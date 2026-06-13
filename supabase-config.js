076dfc295c5aea232be71c993b037a48701da352
mells012 <mellstephania@gmail.com>
2026-06-12 20:25:45 -0500

    fix: update Supabase URL to remove unnecessary endpoint

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 // Rellena estos dos valores con los de tu proyecto en supabase.com
 // Settings → API → Project URL  y  anon public key
-window.SUPABASE_URL      = "https://hwzqsubpzptadpfogypc.supabase.co/rest/v1/";
+window.SUPABASE_URL      = "https://hwzqsubpzptadpfogypc.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_JU9U-cb7q2fDu6W911jVdQ_en7iuZ2N";
